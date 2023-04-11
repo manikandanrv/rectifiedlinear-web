f0afd9e77aa95773c263b5ab338bc6587214c5b3
Mani Rangaswamy <mani@colaberry.com>
2023-04-11 10:10:01 +0530

    Fixed slug issue
    Fixed slug issue

--- src/pages/post/[slug].js
+++ src/pages/post/[slug].js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useRouter} from 'next/router'
 
 import {getPosts, getPostDetails} from '../../services'
-import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader} from '../../components' 
+import {PostDetail, Categories, PostWidget,  Loader} from '../../components' 
 
 const PostDetails = ({post}) => {
   const router = useRouter();
